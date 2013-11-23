9bcc1b266009ee042f0bf87f33078fb9d4a885d1
陆议 <fruit.wj@taobao.com>
2013-11-23 20:42:18 +0800

    更新

---
+++ website/app/app.js
@@ -0,0 +1,59 @@
+﻿KISSY.config({
+    map:[
+        [/(.+app\/.+)-min.js(\?[^?]+)?$/, "$1.js$2"]
+    ],
+    packages:[
+        {
+            name:"app",
+            tag:"20131123",
+            path:"./",
+            charset:"utf-8"
+        }
+    ]
+});
+
+KISSY.ready(function(){
+    KISSY.use('app/sitemap, switchable', function(S, SiteMap, Switchable){
+        var $ = S.all;
+
+        function Site(){
+            this.renderUI();
+        }
+
+        S.augment(Site, {
+            renderUI: function(){
+                var navEl = $('.nav');
+                var cntEl = $('.cnt');
+                var navTpl = $('#J_nav_tpl').html();
+                var cntTpl = $('#J_cnt_tpl').html();
+
+                navEl.html(Mustache.render(navTpl, {siteMap: SiteMap}));
+                cntEl.html(Mustache.render(cntTpl, {siteMap: SiteMap}));
+
+                this.bindUI();
+            },
+            bindUI: function(){
+                var tab = Switchable.Tabs('#J_tabs', {
+                    navCls: 'nav',
+                    contentCls: 'cnt',
+                    triggerType : 'click',
+                    activeTriggerCls: 'active'
+                });
+
+                tab.on('beforeSwitch', function(ev){
+                    var panel = $(tab.panels[ev.toIndex]);
+                    var img = panel.all('img');
+
+                    img.each(function(item){
+                        if (item.hasAttr('data-image')) {
+                            item.attr('src', item.attr('data-image'));
+                            item.removeAttr('data-image');
+                        }
+                    });
+                });
+            }
+        });
+
+        new Site();
+    });
+});
---
+++ website/app/components/searchengin/index.js
@@ -0,0 +1,107 @@
+﻿/**
+ * 搜索引擎
+ * time 2011-11-13
+ */
+KISSY.add('components/searchengin/index', function(S){
+
+    function searchengin(){
+        this._init();
+    }
+
+    S.augment(searchengin, {
+        
+        _init : function(){
+            this.engin = {
+                'baidu' : ['word','http://www.baidu.com/s'],
+                'google' : ['q','http://www.google.com.hk/search'],
+                'bing' : ['q','http://cn.bing.com/search']
+            }
+
+            this.con = S.one('#J_selectPanel');
+            this.hand = S.one('#J_selectHandle');
+            this.form = S.one('#J_searchForm');
+            this.input = S.one('#J_searchInput');
+            this.submit = S.one('#J_searchSubmit');
+
+            this.bindUI();
+        },
+
+        bindUI : function(){
+            var self = this;
+
+            this.hand.on("click", function(e) {
+                self.toggle();
+            });
+            S.one("body").on("click", function(e) {
+                if (S.one(e.target).attr("id") == 'J_selectPanel') {
+                    return;
+                }
+                if (S.one(e.target).attr("id") == 'J_selectHandle') {
+                    return;
+                }
+                if (S.one(e.target).parent().attr("id") == 'J_selectHandle') {
+                    return;
+                }
+                if (self.con.hasClass('ks-hidden')) {
+                    return;
+                }
+                var B = function(D, E) {
+                    return  D[0] > E[0].x &&
+                            D[0] < E[1].x && 
+                            D[1] > E[0].y && 
+                            D[1] < E[1].y;
+                };
+                if (!B(
+                        [e.pageX, e.pageY], 
+                        [{x: self.con.offset().left,y: self.con.offset().top}, {x: self.con.offset().left + self.con.width(),y: self.con.offset().top + self.con.height()}]
+                )) {
+                    self.hide();
+                }
+            });
+
+            this.con.all('li').each(function(node){
+                node.on('click',function(){
+                    self.change(node);  
+                });
+            });
+
+            this.submit.on('click',function(e){
+                e.preventDefault();
+                self.form[0].submit();
+            });
+            
+        },
+
+        change : function(node){
+            var self = this;
+
+            //切换className
+            var curClass = node.attr('class');
+            var prevClass = this.hand.attr('class');
+            this.hand.replaceClass(prevClass,curClass);
+            this.hide();
+
+            //切换表单
+            this.input.attr('name',self.engin[curClass][0]);
+            this.form.attr('action',self.engin[curClass][1]);
+        },
+
+        toggle : function() {
+            if (this.con.hasClass('ks-hidden')) {
+                this.show();
+            } else {
+                this.hide();
+            }
+        },
+
+        show : function(){
+            this.con.removeClass('ks-hidden');
+        },
+
+        hide : function(){
+            this.con.addClass('ks-hidden');        
+        }
+    });
+
+    return searchengin;
+});
